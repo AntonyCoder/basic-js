a84116135a22a0749ce929060343cfba8f341cfc
AntonyCoder <lysenko.tosha@gmail.com>
2021-02-24 16:07:40 +0300

    feat: add solution dream-team, hanoi-tower, recursive-depth, transform-array, what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
+   if (Array.isArray(members)) {
+    let newArr = [];
+    let result = '';
+    members.forEach(element => {
+      if (typeof element === "string") {
+        for (let i = 0; i < element.length; i += 1) {
+          if (element[i] !== ' ') {
+            newArr.push(element[i].toUpperCase());
+            break;
+          }
+        }
+      }
+    });
+    newArr.sort();  
+    result = newArr.join('');
+    return result;
+  } else {
+    return false;
+  }
 };
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+  if (typeof arr !== 'object') throw new Error('Error');
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === '--discard-next') {
+      i += 1;
+    } else if (arr[i] === '--discard-prev') {
+      if (i !== 0 && arr[i-2] !== '--discard-next') {
+        newArr.pop(arr[i-1]);
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1) {
+        newArr.push(arr[i+1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && arr[i-2] !== '--discard-next') {
+        newArr.push(arr[i-1]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
